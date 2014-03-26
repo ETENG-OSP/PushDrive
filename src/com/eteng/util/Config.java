1632d30644e5c72ff37c6c71553d739d7f0b8a5e
GG <gerald_721@hotmail.com>
2014-03-26 04:46:51 -0700

    fix to etframework

--- src/com/eteng/util/Config.java
+++ src/com/eteng/util/Config.java
@@ -1,26 +1,35 @@
 package com.eteng.util;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.json.JSONObject;
+
+import et.common.util.ClassUtility;
 
 public class Config {
 	
 	private static JSONObject config;
+	private static final String FILE = "config/push.json";
+	
+	private static final int BUFFER_SIZE = 32;
 	
 	private static void init() throws IOException {
 		if (config == null) {
-			File file = new File("config/push.json");
-			
-			FileInputStream inputStream = new FileInputStream(file);
-			int length = (int) file.length();
-			byte[] buffer = new byte[length];
-			inputStream.read(buffer);
-			inputStream.close();
-			
-			config = new JSONObject(new String(buffer));
+		    InputStream input = ClassUtility.loadResource(FILE);
+		    ByteArrayOutputStream output = new ByteArrayOutputStream();
+		    byte[] buffer = new byte[BUFFER_SIZE];
+		    while (true) {
+		        int len = input.read(buffer);
+		        if (len < 0) {
+		            break;
+		        }
+		        output.write(buffer, 0, len);
+		    }
+		    
+		    String text = new String(output.toByteArray());
+		    config = new JSONObject(text);
 		}
 	}
 	
